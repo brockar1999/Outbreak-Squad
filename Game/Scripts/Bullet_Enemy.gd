22d8bac26bf9b563ebefd4867c487390c94ad022
Allie Brock <arbrock1999@gmail.com>
2020-04-29 02:27:44 -0400

    we can shoot now

--- Game/Scripts/Bullet_Enemy.gd
+++ Game/Scripts/Bullet_Enemy.gd
@@ -1,14 +1,22 @@
 extends "res://Scripts/Bullet.gd"
 
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
 func _on_Bullet_area_entered(area):
-	if area.is_in_group("enemy"):
+	if area.is_in_group("player"):
 		area.health -= 1
 		queue_free()
 	pass # Replace with function body.
